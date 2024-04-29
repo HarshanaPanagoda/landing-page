b3035e0a83fe529deaaa20adc092fb8cd7341bba
Harshana Panagoda <158323210+HarshanaPanagoda@users.noreply.github.com>
2024-04-29 22:47:54 +0530

    Add files via upload
    first commit

---
+++ Insync landing page/script.js
@@ -0,0 +1,40 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+
+
+/*scroll button behavior */
+
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  if (window.scrollY > 300) {
+    // Show backToTopButton
+    if (!backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnExit");
+      backToTopButton.classList.add("btnEntrance");
+      backToTopButton.style.display = "block";
+    }
+  } else {
+    // Hide backToTopButton
+    if (backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnEntrance");
+      backToTopButton.classList.add("btnExit");
+      setTimeout(function () {
+        backToTopButton.style.display = "none";
+      }, 250);
+    }
+  }
+}
+
+backToTopButton.addEventListener("click", BackToTop);
+function BackToTop() {
+  window.scrollTo(0, 0);
+}
+
